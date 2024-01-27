a75c40ef7db18816e63ef7919893b8ed2610eabb
Ivan Larin <n.larivanko@gmail.com>
2024-01-27 13:47:02 +0200

    minor change

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,5 +1,3 @@
-import { easing } from "maath";
-import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 import { useRef } from "react";
 
@@ -10,14 +8,14 @@ const Backdrop = () => {
       ref={shadowsRef}
       temporal
       frames={60}
-      alphaTest={0.3}
+      alphaTest={0.25}
       scale={10}
       rotation={[Math.PI / 2, 0, 0]}
-      position={[0, 0, -0.14]}
+      position={[0, 0, -0.2]}
     >
       <RandomizedLight
         amount={4}
-        radius={9}
+        radius={6}
         intensity={0.55}
         ambient={0.25}
         position={[5, 5, -10]}
